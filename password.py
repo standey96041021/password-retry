8712664e0161bf4ae3766182391ce60417643cc2
Yan Cheng <standey12345@gmail.com>
2021-03-22 16:40:36 +0800

    finish version

--- password.py
+++ password.py
@@ -1,14 +1,20 @@
-i=0
-j=2
-while True:
-    password = input('請輸入密碼:')
-    if password == ('a123456'):
-        print('登入成功!')
-        break
-    elif i<2:
-        print('密碼錯誤! 還有', j, '次機會')
-        j=j-1
-        i=i+1
+#密碼重試程式
+#password = 'a123456'
+#讓使用者重複輸入密碼
+#最多輸入3次
+#如果正確 就印出 "登入成功!"
+#如果不正確 就印出 "密碼錯誤! 還有__次機會!"
+password = 'a123456'
+i = 3 #剩餘機會
+while i > 0:
+    i = i - 1
+    pwd = input('請輸入密碼:')
+    if pwd == password:
+        print('登入成功')
+        break # 逃出迴圈
     else:
-        print('登入失敗')
-        break
+        print('密碼錯誤!')
+        if i > 0:
+            print('還有', i, '次機會')
+        else:
+            print('沒機會嘗試了! 要鎖帳號了啦!')    
